fc1d9b20c72b2d77dbbac08cab6498264bb05639
Your Name <crrojasserrano@gmail.com>
2021-06-16 15:45:18 -0400

    models fixed

--- apps/secr_app/models.py
+++ apps/secr_app/models.py
@@ -14,14 +14,15 @@ class Lawsuit_State(models.Model):
 
 
 class Defendant(models.Model): 
-    first_name1 = models.CharField(max_length=45, blank=False, null=False)
-    first_name2 = models.CharField(max_length=45, blank=False, null=False)
-    last_name1 = models.CharField(max_length=45, blank=False, null=False)
-    last_name2 = models.CharField(max_length=45, blank=False, null=False)
-    address = models.CharField(max_length=255, blank=False, null=False)
-    rut = models.CharField(max_length=10, blank=False, null=False)
-    email = models.CharField(max_length=45, blank=False, null=False)
-    password = models.CharField(max_length=100, blank=False, null=False)
+    first_name1 = models.CharField(max_length=45, blank=False, null=False) #este campo
+    first_name2 = models.CharField(max_length=45, blank=False, null=False) #este campo
+    last_name1 = models.CharField(max_length=45, blank=False, null=False) #este campo
+    last_name2 = models.CharField(max_length=45, blank=False, null=False) #este campo
+    address = models.CharField(max_length=255, blank=False, null=False) #este campo
+    rut = models.CharField(max_length=10, blank=False, null=False) #este campo
+
+    # email = models.CharField(max_length=45, blank=False, null=False)
+    # password = models.CharField(max_length=100, blank=False, null=False)
     defendant_created_by = models.ForeignKey(Administrator, related_name="user_create_defendant", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -29,13 +30,14 @@ class Defendant(models.Model):
 
 
 class Lawsuit(models.Model):
-    num_promissory_notes = models.CharField(max_length=45)
-    final_date = models.DateField()
-    mount_to_pay = models.IntegerField()
-    num_operation = models.CharField(max_length=255)
-    suscription_date= models.DateField()
-    demand_amount= models.IntegerField()
-    cause_rol = models.CharField(max_length=45)
+    num_promissory_notes = models.CharField(max_length=45)   #este campo
+    final_date = models.DateField()  #este campo
+    mount_to_pay = models.IntegerField() #este campo
+    num_operation = models.CharField(max_length=255) #este campo
+    suscription_date= models.DateField() #este campo
+    demand_amount= models.IntegerField() #este campo
+    cause_rol = models.CharField(max_length=45) 
+    
     current_defendant = models.ForeignKey(Defendant, related_name="lawsuits", on_delete = models.CASCADE)
     current_demand_state = models.ForeignKey(Lawsuit_State, related_name="lawsuits", on_delete = models.CASCADE)
     current_court = models.ForeignKey(Lawsuit_State, related_name="lawsuits", on_delete = models.CASCADE)
